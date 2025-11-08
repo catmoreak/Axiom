0669f9da86dc90c1b7572e93b7b173d32e8060fa
catmoreak <72437057+catmoreak@users.noreply.github.com>
2025-11-08 20:45:14 +0530

    [feat] New loading screen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,19 +3,43 @@ import React from 'react';
 export default function LoadingScreen() {
   return (
     <div className="loading-screen">
-
-        
       <div className="loading-container">
-        <div className="molecule-spinner">
-          <div className="orbit orbit1"></div>
-          <div className="orbit orbit2"></div>
-          <div className="atom atom1"></div>
-          <div className="atom atom2"></div>
-          <div className="atom atom3"></div>
+        <div className="orbital-system">
+          
 
+          <div className="nucleus">
+            <div className="proton"></div>
+            <div className="proton"></div>
+            <div className="neutron"></div>
+          </div>
+          
+          
+          <div className="orbital-ring ring-1">
+            <div className="electron electron-1"></div>
+          </div>
+          
+          <div className="orbital-ring ring-2">
+            <div className="electron electron-2"></div>
+            <div className="electron electron-3"></div>
+          </div>
+          
+          <div className="orbital-ring ring-3">
+            <div className="electron electron-4"></div>
+          </div>
+          
+          
+          <div className="energy-field"></div>
+          <div className="glow-effect"></div>
         </div>
         
-        
+        <div className="loading-text">
+         
+          <div className="progress-dots">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
       </div>
     </div>
   );
