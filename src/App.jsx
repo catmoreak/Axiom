f467bdd7f34b75eac208f5161c555208d6923d35
catmoreak <72437057+catmoreak@users.noreply.github.com>
2025-11-08 09:27:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Chemical Reactions Explorer</h1>
+      <p>Welcome! Select a reaction to learn more.</p>
+    </div>
+  );
+}
+
+
+
+
