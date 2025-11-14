fb3255dd64dbbf056c0fb8396486af9829d30d28
catmoreak <72437057+catmoreak@users.noreply.github.com>
2025-11-14 18:19:54 +0530

    [fix] added overflow for teh homepage

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { mx_worley_distance } from "three/src/nodes/materialx/lib/mx_noise.js";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
 
-impoert 
+
